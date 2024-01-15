916cc03bae2397ff254caa2ec1991eb82b50b09b
Duane Evans <duanesevans@gmail.com>
2024-01-14 16:19:50 -0800

    Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
+import Head from "next/head"
 import QueryProvider from "./QueryProvider"
 import "./globals.css"
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "3-2-1 NHL",
+  icons:
+    "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏒</text></svg>",
+}
 
 export default function RootLayout({
   children,
