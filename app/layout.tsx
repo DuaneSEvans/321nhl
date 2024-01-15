867edd8e0221b4e4578e3b34fda479ea74a47adb
Duane Evans <duanesevans@gmail.com>
2024-01-14 16:21:01 -0800

    Tweak to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,9 @@ import "./globals.css"
 import type { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: "3-2-1 NHL",
+  title: "3-2-1 NHL Standings",
+  description:
+    "The NHL Standings if the league followed a 3-2-1-0 point system",
   icons:
     "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏒</text></svg>",
 }
