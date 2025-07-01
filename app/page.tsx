8f508e06dbc78ace8727f0109388df01777c4eb9
Duane Evans <duanesevans@gmail.com>
2025-07-01 11:02:53 -0700

    avoid Vercel caching

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,17 @@ import { z } from "zod"
 import Standings from "./components/Standings"
 import { RawStandingSchema, Standing } from "./shared"
 
+// Add timestamp to avoid vercel caching issue https://github.com/vercel/vercel/discussions/10117
 export default async function Home() {
-  const res = await fetch("https://api-web.nhle.com/v1/standings/now")
+  // https://gitlab.com/dword4/nhlapi/-/blob/master/new-api.md?ref_type=heads#standings
+  const res = await fetch(
+    "https://api-web.nhle.com/v1/standings/now?ts=" + Date.now(),
+    {
+      cache: "no-store",
+    }
+  )
   const data = await res.json()
+  console.log(data.standings[0])
 
   const parsedStandings = z.array(RawStandingSchema).parse(data.standings)
 
